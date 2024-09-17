a6e922b0f8d8f1cd6e925e81b64ef6982fd4504e
zydnet <da9592@srmist.edu.in>
2024-09-17 15:39:57 +0530

    changes in colours made

--- script.js
+++ script.js
@@ -29,6 +29,69 @@ gsap.from("#home p, #home a", {
     ease: "power3.out"
 });
 
+const chatinput=document.querySelector(".chat-input textarea");
+const sendchatbtn=document.querySelector(".chat-input span");
+const chatbox=document.querySelector(".chatbox");
+const chatbotToggler=document.querySelector(".chatbot-toggler")
+
+
+let usermessage;
+const createchatli=(message, classname)=>{
+    const chatli=document.createElement("li");
+    chatli.classList.add("chat",classname);
+    let chatcontent=classname ==="outgoing" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    chatli.innerHTML=chatcontent;
+    chatli.querySelector("p").textContent=message;
+    return chatli;
+}
+
+const generateResponse=(incomingChatLi)=>{
+    const au="http://127.0.0.1:5000/query"
+    const messageElement=incomingChatLi.querySelector("p")
+
+    const reop={
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt: usermessage })
+    }
+    fetch(au,reop).then(res=>res.json()).then(data=>{
+        console.log(data) 
+    }).catch((error)=>{
+        console.log(error);
+        
+    }).finally(()=>chatbox.scrollTo(0,chatbox.scrollHeight));
+}
+
+
+
+
+const handlechat=()=>{
+    usermessage=chatinput.value.trim();
+    if(!usermessage) return;
+    chatinput.value="";
+
+    chatbox.appendChild(createchatli(usermessage,"outgoing"));
+    chatbox.scrollTo(0,chatbox.scrollHeight);
+    setTimeout(()=>{
+        const incomingChatLi=createchatli("Thinking...","incoming")
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0,chatbox.scrollHeight);
+        generateResponse(incomingChatLi);
+    },600);
+}
+sendchatbtn.addEventListener("click",handlechat)
+chatbotToggler.addEventListener("click",()=> document.body.classList.toggle("show-chatbot"))
+
+
+
+
+
+
+
+
+
 // Disease data
 const diseases = [
     {
